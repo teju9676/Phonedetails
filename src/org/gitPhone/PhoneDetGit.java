627e68ffb884faa68131b48d3ccc6d4c78741c20
teju9676 <tejaswiniteju1707@gmail.com>
2023-01-19 15:04:44 +0530

    phone details push

---
+++ src/org/gitPhone/PhoneDetGit.java
@@ -0,0 +1,36 @@
+package org.gitPhone;
+
+
+
+public class PhoneDetGit {
+	private void phoneName() {
+		System.out.println("Phone name is redmi");
+
+	}
+	private void phoneImeiNum() {
+		System.out.println("Imei num 1234567890");
+
+	}
+	private void phoneCamera() {
+		System.out.println("Camera 64mega pixel");
+
+	}
+	private void phoneStorage() {
+		System.out.println("Storage 256GB");
+		
+	}
+	private void phoneOs() {
+		System.out.println("OS Andriod");
+
+	}
+	public static void main(String[] args) {
+		PhoneDetGit a=new PhoneDetGit();
+		
+		a.phoneName();
+		a.phoneImeiNum();
+		a.phoneCamera();
+		a.phoneStorage();
+		a.phoneOs(); 
+	}
+
+}
